95f1db7a9d6ac3e4934ff439dac5531641331ceb
Philipp Schoneville <p.schoneville@gmx.net>
2024-02-20 01:06:09 +0100

    feat: Use pandas

--- src/distance_matrix.py
+++ src/distance_matrix.py
@@ -1,41 +1,55 @@
-import json
 from typing import Callable
+import itertools
 
+import pandas as pd
 import numpy as np
 from positions import generate_positions
-from similarity import jaro_hamming
+from similarity import sorensen_dice_hamming
 
 
 class DistanceMatrix:
-    def __init__(
-        self, index: dict[str, int], similarity_func: Callable[[str, str], float]
-    ):
-        self.index = index
-        self.matrix = np.array(
-            [[similarity_func(p1, p2) for p2 in index.keys()] for p1 in index.keys()]
+    def __init__(self, index: list[str], similarity_func: Callable[[str, str], float]):
+        similarities = [
+            similarity_func(i, j) for i, j in itertools.product(index, repeat=2)
+        ]
+        # Reshape the results into a DataFrame
+        self.df = pd.DataFrame(
+            np.array(similarities).reshape(len(index), len(index)),
+            index=index,
+            columns=index,
         )
 
-    def set_value(self, row_key, col_key, value):
-        try:
-            self.matrix[self.index[row_key], self.index[col_key]] = value
-        except KeyError:
-            raise ValueError("One or both keys not found in the index.")
+    def set_value(self, row_key: str, col_key: str, value: float) -> None:
+        self.df.loc[row_key, col_key] = value
 
-    def get_value(self, row_key, col_key):
-        try:
-            return self.matrix[self.index[row_key], self.index[col_key]]
-        except KeyError:
-            raise ValueError("One or both keys not found in the index.")
+    def get_value(self, row_key: str, col_key: str) -> float:
+        return self.df.loc[row_key, col_key]  # type: ignore
+
+    def get_column(self, col_key: str) -> pd.Series:
+        return self.df[col_key]
+
+    def get_row(self, row_key: str) -> pd.Series:
+        return self.df.loc[row_key]
+
+    def get_sorted_values(self) -> pd.DataFrame:
+        """Convert the DataFrame to a Series with the index (stack) and the values."""
+        return (
+            self.df.unstack()  # Convert to long form
+            .reset_index()  # Reset index to get column names
+            .rename(columns={"level_0": "white", "level_1": "black", 0: "distance"})
+            .sort_values(by="distance")
+            .reset_index(drop=True)
+        )
 
 
 if __name__ == "__main__":
-    # the index is a dictionary that maps the position in format 'rnbqkbnr' to an integer index
-    index = {"".join(pos): i for i, pos in enumerate(generate_positions())}
+    index = ["".join(pos) for pos in generate_positions()]
+    distances = DistanceMatrix(index=index, similarity_func=sorensen_dice_hamming)
+    sorted_values = distances.get_sorted_values()
 
-    distances = DistanceMatrix(index=index, similarity_func=jaro_hamming)
-    print(distances.matrix)
-
-    # save matrix to JSON
-    # together with the index
-    with open("distances.json", "w") as f:
-        json.dump({"index": index, "matrix": distances.matrix.tolist()}, f)
+    print(distances.df)
+    print(sorted_values)
+    distances.df.to_parquet("distances.parquet")
+    distances.df.to_csv("distances.csv")
+    sorted_values.to_parquet("sorted_values.parquet")
+    sorted_values.to_csv("sorted_values.csv")
