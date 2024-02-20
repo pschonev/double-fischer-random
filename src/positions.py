caf137baa4ea6203f1b42b8c3e578ca50e6b5a6b
Philipp Schoneville <p.schoneville@gmx.net>
2024-02-20 01:07:22 +0100

    chore: Move code

--- src/positions.py
+++ src/positions.py
@@ -1,5 +1,6 @@
 from itertools import combinations
 from typing import Generator
+from utils import logger
 
 
 def generate_positions() -> Generator[list[str], None, None]:
@@ -26,3 +27,24 @@ def generate_positions() -> Generator[list[str], None, None]:
                         starting_position.index("r", starting_position.index("r") + 1)
                     ] = "k"
                     yield starting_position
+
+
+def is_valid_chess960_position(sequence: str) -> bool:
+    valid = True
+    if len(sequence) != 8:
+        logger.error(f"Invalid sequence length {len(sequence)}")
+        valid = False
+    if sorted(sequence) != ["b", "b", "k", "n", "n", "q", "r", "r"]:
+        logger.error(f"Invalid piece counts or pieces in sequence {sequence}")
+        valid = False
+    # check if there is on b on odd index and one b on even index
+    if sequence.index("b") % 2 == sequence.rindex("b") % 2:
+        logger.error(f"Invalid sequence, bishops on same color in {sequence}")
+        valid = False
+    # check if k between both r values
+    if sequence.index("k") < sequence.index("r") or sequence.index(
+        "k"
+    ) > sequence.rindex("r"):
+        logger.error(f"Invalid sequence, king not between rooks in {sequence}")
+        valid = False
+    return valid
--- src/utils.py
+++ src/utils.py
@@ -2,24 +2,3 @@ import logging
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
-
-
-def is_valid_chess960(sequence: str) -> bool:
-    valid = True
-    if len(sequence) != 8:
-        logger.error(f"Invalid sequence length {len(sequence)}")
-        valid = False
-    if sorted(sequence) != ["b", "b", "k", "n", "n", "q", "r", "r"]:
-        logger.error(f"Invalid piece counts or pieces in sequence {sequence}")
-        valid = False
-    # check if there is on b on odd index and one b on even index
-    if sequence.index("b") % 2 == sequence.rindex("b") % 2:
-        logger.error(f"Invalid sequence, bishops on same color in {sequence}")
-        valid = False
-    # check if k between both r values
-    if sequence.index("k") < sequence.index("r") or sequence.index(
-        "k"
-    ) > sequence.rindex("r"):
-        logger.error(f"Invalid sequence, king not between rooks in {sequence}")
-        valid = False
-    return valid
