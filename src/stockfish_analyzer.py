9a2a81cfe8b2a01bf41fbfac6627891974ba2b6d
Philipp Schoneville <p.schoneville@gmx.net>
2024-04-21 22:38:44 +0200

    feat: Add analyzer

---
+++ src/stockfish_analyzer.py
@@ -0,0 +1,184 @@
+from collections.abc import Callable, Generator
+from dataclasses import asdict, dataclass
+from pathlib import Path
+from typing import Any, Self
+
+from distance_matrix import DistanceMatrix
+from stockfish import Stockfish
+
+
+@dataclass
+class AnalysisConfig:
+    stockfish_depth: int
+    analysis_depth: int
+    num_top_moves: int
+    balanced_threshold: int
+
+
+@dataclass
+class WDL:
+    win: int
+    draw: int
+    loss: int
+
+
+@dataclass
+class TopMoveEval:
+    move: str
+    centipawn: int
+    mate: bool
+
+    def __post_init__(self) -> None:
+        self.mate = self.mate is not None
+
+
+@dataclass
+class MoveEval:
+    fen: str
+    halfmove: int
+    wdl: WDL
+    top_moves: list[TopMoveEval]
+    analysis_cfg: AnalysisConfig
+
+    def to_row(self) -> dict[str, Any]:
+        top_moves = {f"top_move_{i}": m.move for i, m in enumerate(self.top_moves)}
+        top_moves_centipawn = {
+            f"top_move_{i}_centipawn": m.centipawn for i, m in enumerate(self.top_moves)
+        }
+        return {
+            "fen": self.fen,
+            "move": self.halfmove // 2 + 1,
+            "turn": "white" if self.halfmove % 2 == 0 else "black",
+            "win": self.wdl.win,
+            "draw": self.wdl.draw,
+            "loss": self.wdl.loss,
+            **asdict(self.analysis_cfg),
+            **top_moves,
+            **top_moves_centipawn,
+        }
+
+
+@dataclass
+class FirstMoveEval(MoveEval):
+    distance: float
+
+
+@dataclass
+class PositionResult:
+    fen: str
+    wdl: WDL
+    centipawn: int
+    sharpness_score: float
+    distance: float
+
+    @classmethod
+    def from_move_eval(
+        cls,
+        first_move_eval: FirstMoveEval,
+        deepest_moves_eval: list[MoveEval],
+        balanced_threshold: int,
+    ) -> Self:
+        """Create a PositionResult from a FirstMoveEval and a list of MoveEval.
+        The sharpeness is calculated by getting the top moves from all the deepest analyzed moves and
+        checking how many of them are below the balanced threshold.
+        """
+        top_moves = [
+            top_move for move in deepest_moves_eval for top_move in move.top_moves
+        ]
+        sharpness_score = sum(
+            1
+            for top_move in top_moves
+            if (not top_move.mate) and (top_move.centipawn < balanced_threshold)
+        ) / len(top_moves)
+        initial_centipawn = max(
+            first_move_eval.top_moves, key=lambda x: x.centipawn
+        ).centipawn
+        return cls(
+            fen=first_move_eval.fen,
+            wdl=first_move_eval.wdl,
+            centipawn=initial_centipawn,
+            sharpness_score=sharpness_score,
+            distance=first_move_eval.distance,
+        )
+
+
+def convert_starting_positions_to_fen(white: str, black: str) -> str:
+    """Convert the starting positions to a FEN string.
+
+    Args:
+        white: The starting position of the white pieces
+        black: The starting position of the black pieces
+
+    Returns:
+        The FEN string of the starting positions
+    """
+    return f"{black.lower()}/pppppppp/8/8/8/8/PPPPPPPP/{white.upper()} w KQkq - 0 1"
+
+
+@dataclass
+class StockfishAnalyzer:
+    stockfish: Stockfish
+    distance_matrix: DistanceMatrix
+
+    cfg: AnalysisConfig
+    sample_strategies: list[Callable[[], tuple[str, str]]]
+
+    def _get_move_analysis(self, fen: str, halfmove: int) -> MoveEval:
+        self.stockfish.set_fen_position(fen)
+
+        wdl = self.stockfish.get_wdl_stats()
+        if not wdl:
+            msg = "No WDL stats found"
+            raise ValueError(msg)
+
+        return MoveEval(
+            fen=fen,
+            halfmove=halfmove,
+            wdl=WDL(*wdl),
+            top_moves=[
+                TopMoveEval(**move)
+                for move in self.stockfish.get_top_moves(self.cfg.num_top_moves)
+            ],
+            analysis_cfg=self.cfg,
+        )
+
+    def analyze_moves_from_position(
+        self,
+        fen: str,
+        halfmove: int,
+    ) -> Generator[MoveEval, None, None]:
+        move_eval = self._get_move_analysis(fen, halfmove)
+        yield move_eval
+
+        if halfmove < (self.cfg.analysis_depth * 2 - 1):
+            for top_move in move_eval.top_moves:
+                self.stockfish.set_fen_position(fen)
+                new_fen = self.stockfish.make_moves_from_current_position(
+                    [top_move.move]
+                )
+                if new_fen is None:
+                    msg = "Invalid move"
+                    raise ValueError(msg)
+                yield from self.analyze_moves_from_position(new_fen, halfmove + 1)
+
+    def get_new_position_fen(self) -> str:
+        starting_positions = self.distance_matrix.get_weighted_random_sample()
+        return convert_starting_positions_to_fen(*starting_positions)
+
+
+if __name__ == "__main__":
+    cfg = AnalysisConfig(
+        stockfish_depth=30,
+        analysis_depth=2,
+        num_top_moves=5,
+        balanced_threshold=100,
+    )
+    analyzer = StockfishAnalyzer(
+        Stockfish(
+            depth=cfg.stockfish_depth,
+            parameters={"UCI_Chess960": "true", "Hash": 2048, "Threads": 7},
+        ),
+        DistanceMatrix.from_parquet(Path("data/distance_matrix.parquet")),
+        cfg=cfg,
+        sample_strategies=[],
+    )
