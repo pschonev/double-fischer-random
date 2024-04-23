f162f24e6f3bdb039165d32a8175963b8fe8036a
Philipp Schoneville <p.schoneville@gmx.net>
2024-04-23 20:08:44 +0200

    chore: Add time to logger

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import logging
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+)
