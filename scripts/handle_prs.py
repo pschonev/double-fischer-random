c5555527786b249e9ec71551effd7825982a3202
Philipp Schoneville <p.schoneville@gmx.net>
2024-12-13 00:29:50 +0100

    wip(collab): Finalize first version of Github Actions workflow

--- scripts/handle_prs.py
+++ scripts/handle_prs.py
@@ -1,48 +1,61 @@
+from dataclasses import dataclass
 import datetime
 import json
 import logging
-import os
-import subprocess
-from http import HTTPStatus
-from pathlib import Path
+from typing import Any, Self
 
-import pandas as pd
-import requests
+import pyarrow.json as pj
+import pyarrow.parquet as pq
 
-# Configure logging
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+from scripts.utils import (
+    get_prs,
+    is_collaborator,
+    add_label,
+    remove_label,
+    run_command,
+    close_pr,
+    PRAnalysisLabel,
+    RESULTS_NDJSON,
+    RESULTS_PARQUET,
+    POSITIONS_FILE,
+    WIP_FILE,
 )
 
-# Global variables
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
-REPO_OWNER = "pschonev"
-REPO_NAME = "double-fischer-random"
-API_URL = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}"
-HEADERS = {"Authorization": f"token {GITHUB_TOKEN}"}
+STALE_HOURS = 6  # hours until PR is considered stale
 
 
-def get_prs() -> dict:
-    response = requests.get(f"{API_URL}/pulls", headers=HEADERS, timeout=10)
-    response.raise_for_status()
-    return response.json()
+@dataclass
+class AnalysisResult:
+    branch_name: str
+    pr_number: str
+    position: str
+    result: dict[str, Any]
 
-
-def is_collaborator(username: str) -> bool:
-    response = requests.get(
-        f"{API_URL}/collaborators/{username}", headers=HEADERS, timeout=10
-    )
-    return response.status_code == HTTPStatus.NO_CONTENT
+    @classmethod
+    def from_result(
+        cls,
+        pr_branch_name: str,
+        pr_number: str,
+        result: dict[str, Any],
+    ) -> Self:
+        return cls(
+            branch_name=pr_branch_name,
+            pr_number=pr_number,
+            position=result["position"],
+            result=result,
+        )
 
 
 def process_prs() -> None:
     try:
         prs = get_prs()
+        analysis_results: list[AnalysisResult] = []
         for pr in prs:
             try:
                 pr_number = pr["number"]
                 pr_user = pr["user"]["login"]
                 pr_labels = [label["name"] for label in pr["labels"]]
+                pr_branch_name = pr["head"]["ref"]
                 pr_created_at = datetime.datetime.strptime(
                     pr["created_at"], "%Y-%m-%dT%H:%M:%SZ"
                 ).astimezone(datetime.UTC)
@@ -50,143 +63,131 @@ def process_prs() -> None:
                 # Remove stale PRs
                 if datetime.datetime.now(
                     tz=datetime.UTC
-                ) - pr_created_at > datetime.timedelta(hours=6):
+                ) - pr_created_at > datetime.timedelta(hours=STALE_HOURS):
                     close_pr(pr_number)
                     continue
 
                 # Process "analyzed" PRs
-                if "analyzed" in pr_labels:
+                if PRAnalysisLabel.ANALYZED in pr_labels:
                     if is_collaborator(pr_user):
-                        merge_results(pr_number)
+                        analysis_results.append(
+                            AnalysisResult.from_result(
+                                pr_branch_name=pr_branch_name,
+                                pr_number=pr_number,
+                                result=collect_analysis_results(pr_branch_name),
+                            )
+                        )
                     else:
-                        add_label(pr_number, "ready_to_validate")
-                        remove_label(pr_number, "analyzed")
+                        add_label(pr_number, PRAnalysisLabel.READY_FOR_VALIDATION)
+                        remove_label(pr_number, PRAnalysisLabel.ANALYZED)
 
                 # Process "validated" PRs
-                if "validated" in pr_labels:
-                    merge_results(pr_number)
+                if PRAnalysisLabel.VALIDATED in pr_labels:
+                    analysis_results.append(
+                        AnalysisResult.from_result(
+                            pr_branch_name=pr_branch_name,
+                            pr_number=pr_number,
+                            result=collect_analysis_results(pr_branch_name),
+                        )
+                    )
 
             except Exception:
                 logging.exception(f"Error processing PR #{pr_number}")
-                add_label(pr_number, "error")
-    except Exception:
+                add_label(pr_number, PRAnalysisLabel.ERROR)
+
+        try:
+            push_analysis_results(analysis_results)
+            convert_ndjson_to_parquet()
+            for result in analysis_results:
+                close_pr(result.pr_number)
+        except Exception as e:
+            logging.exception("Error pushing analysis results")
+            raise e
+    except Exception as e:
         logging.exception("Error fetching PRs")
+        raise e
 
 
-def close_pr(pr_number: str) -> None:
+def collect_analysis_results(branch_name: str) -> dict[str, Any]:
+    """Collect the analysis results for a given branch
+
+    Args:
+        branch_name: The branch name
+
+    Returns:
+        The analysis results
+
+    Raises:
+        Exception: If an error occurs while collecting the analysis results
+    """
     try:
-        requests.patch(
-            f"{API_URL}/pulls/{pr_number}",
-            headers=HEADERS,
-            json={"state": "closed"},
-            timeout=10,
-        ).raise_for_status()
-    except Exception:
-        logging.exception(f"Error closing PR #{pr_number}")
-
-
-def add_label(pr_number: str, label: str) -> None:
-    try:
-        requests.post(
-            f"{API_URL}/issues/{pr_number}/labels",
-            headers=HEADERS,
-            json={"labels": [label]},
-            timeout=10,
-        ).raise_for_status()
-    except Exception:
-        logging.exception(f"Error adding label '{label}' to PR #{pr_number}")
-
-
-def remove_label(pr_number: str, label: str) -> None:
-    try:
-        requests.delete(
-            f"{API_URL}/issues/{pr_number}/labels/{label}",
-            headers=HEADERS,
-            timeout=10,
-        ).raise_for_status()
-    except Exception:
-        logging.exception(f"Error removing label '{label}' from PR #{pr_number}")
-
-
-def run_command(command: list[str]) -> str:
-    try:
-        result = subprocess.run(command, check=True, text=True, capture_output=True)  # noqa: S603
-    except subprocess.CalledProcessError as e:
-        logging.exception(f"Command '{command}' failed with error: {e.stderr}")
-        raise
-    else:
-        return result.stdout
-
-
-def merge_results(pr_number: str) -> None:
-    try:
-        # Fetch the PR details to get the branch name
-        pr_details = requests.get(
-            f"{API_URL}/pulls/{pr_number}",
-            headers=HEADERS,
-            timeout=10,
-        ).json()
-        branch_name = pr_details["head"]["ref"]
-
-        # Checkout the branch and fetch the .wip.json file
+        # Checkout the branch and fetch the latest changes
         run_command(["git", "fetch", "origin", branch_name])
         run_command(["git", "checkout", branch_name])
 
-        # Use pathlib for file paths
-        wip_file_path = Path(".wip.json")
-        ndjson_file_path = Path("result.ndjson")
+        with WIP_FILE.open("r") as file:
+            return json.load(file)
 
-        if wip_file_path.exists():
-            with wip_file_path.open("r") as file:
-                wip_data = json.load(file)
-
-            # Append the data to result.ndjson
-            with ndjson_file_path.open("a") as ndjson_file:
-                json.dump(wip_data, ndjson_file)
-                ndjson_file.write("\n")
-
-            # Remove the .wip.json file
-            wip_file_path.unlink()
-
-            # Commit the changes
-            run_command(["git", "add", "result.ndjson"])
-            run_command(
-                ["git", "commit", "-m", f"Merged analysis data from PR #{pr_number}"]
-            )
-            run_command(["git", "push", "origin", "main"])
-
-            # Merge the PR
-            requests.put(
-                f"{API_URL}/pulls/{pr_number}/merge",
-                headers=HEADERS,
-                json={"commit_title": f"Merge PR #{pr_number}"},
-                timeout=10,
-            ).raise_for_status()
-
-        # Convert result.ndjson to Parquet
-        convert_ndjson_to_parquet(ndjson_file_path)
-
-    except Exception:
-        logging.exception(f"Error merging results for PR #{pr_number}")
-        add_label(pr_number, "error")
+    except Exception as e:
+        logging.exception(f"Error collecting analysis results for branch {branch_name}")
+        raise e
 
 
-def convert_ndjson_to_parquet(ndjson_file_path: Path) -> None:
+def push_analysis_results(analysis_results: list[AnalysisResult]) -> None:
+    """Push the analysis results to the NDJSON file and delete all analyzed positions
+    from the positions.txt file.
+
+    Args:
+        analysis_results: The analysis results to push
+    """
+    run_command(["git", "checkout", "main"])
+    run_command(["git", "pull", "origin", "main"])
+
+    # Append new results to the NDJSON file
+    analyzed_positions: set[str] = set()
+    with RESULTS_NDJSON.open("a") as file:
+        for result in analysis_results:
+            file.write(json.dumps(result.result) + "\n")
+            analyzed_positions.add(result.position)
+
+    # Remove all analyzed positions from the positions file
+    with POSITIONS_FILE.open("r") as file:
+        positions = file.readlines()
+
+    with POSITIONS_FILE.open("w") as file:
+        for position in positions:
+            if position.strip() not in analyzed_positions:
+                file.write(position)
+
+    # Commit the NDJSON file and the positions file
+    run_command(["git", "add", str(RESULTS_NDJSON), str(POSITIONS_FILE)])
+    run_command(
+        [
+            "git",
+            "commit",
+            "-m",
+            f"chore(data): Updated {RESULTS_NDJSON} and {POSITIONS_FILE}",
+        ]
+    )
+    run_command(["git", "push", "origin", "main"])
+
+
+def convert_ndjson_to_parquet() -> None:
     try:
-        # Read the NDJSON file into a DataFrame
-        df_results = pd.read_json(ndjson_file_path, lines=True)
+        # Read the NDJSON file into a Table
+        table = pj.read_json(RESULTS_NDJSON)
 
         # Convert to Parquet
-        parquet_file_path = Path("result.parquet")
-        df_results.to_parquet(parquet_file_path)
+        pq.write_table(table, RESULTS_PARQUET, compression="BROTLI")
 
         # Commit the Parquet file
-        run_command(["git", "add", str(parquet_file_path)])
-        run_command(["git", "commit", "-m", "Updated result.parquet"])
+        run_command(["git", "add", str(RESULTS_PARQUET)])
+        run_command(["git", "commit", "-m", f"chore(data): Updated {RESULTS_PARQUET}"])
         run_command(["git", "push", "origin", "main"])
 
-    except Exception:
+    except Exception as e:
         logging.exception("Error converting NDJSON to Parquet")
+        raise e
 
 
 if __name__ == "__main__":
