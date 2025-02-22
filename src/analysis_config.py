e4f4ff3af09df3aaf531e53e60f494617303c8ba
Philipp Schoneville <ph.schoneville@gmail.com>
2025-02-22 22:56:08 +0100

    refactor: Change config

--- src/analysis_config.py
+++ src/analysis_config.py
@@ -38,10 +38,6 @@ class AnalysisConfig(msgspec.Struct):
 
 class ConfigId(IntEnum):
     XS = 10
-    S = 20
-    M = 30
-    L = 40
-    XL = 50
 
     def __str__(self) -> str:
         return self.name.lower()
