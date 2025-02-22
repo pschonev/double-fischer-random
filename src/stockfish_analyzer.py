1a8210d1f9a2a27c44e9d40a2354b580c8084d4c
Philipp Schoneville <ph.schoneville@gmail.com>
2025-02-22 22:55:32 +0100

    feat: Add analyse function

--- src/stockfish_analyzer.py
+++ src/stockfish_analyzer.py
@@ -1,8 +1,19 @@
 from dataclasses import dataclass
+import logging
+import os
+
 import chess
 import chess.engine
-from src.analysis_config import AnalysisConfig, load_config
+
+from src.analysis_config import AnalysisConfig, load_config, ConfigId
 from src.analysis_results import PositionNode, PositionAnalysis
+from src.positions import get_chess960_position
+
+
+AnalysisTree = PositionNode
+
+logging.getLogger("chess.engine").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -87,7 +98,7 @@ class RecursiveEngineAnalyzer:
 
     def analyse(
         self,
-    ) -> PositionNode:
+    ) -> AnalysisTree:
         """Perform complete position analysis."""
         analysis_tree = self._build_analysis_tree(self.board.copy(), 0)
         if analysis_tree is None:
@@ -96,23 +107,61 @@ class RecursiveEngineAnalyzer:
         return analysis_tree
 
 
-if __name__ == "__main__":
-    # Initialize the chess board with a sample position
-    board = chess.Board()
+def analyse_dfrc_position(
+    white_id: int,
+    black_id: int,
+    cfg_id: ConfigId,
+    engine_path: str = "stockfish",
+    threads: int | None = None,
+    hash_size: int | None = None,
+) -> AnalysisTree:
+    """Perform analysis on a Chess960 position given by unique IDs."""
+    # Get the Chess960 position
+    white, black = get_chess960_position(white_id), get_chess960_position(black_id)
+
+    # Initialize the chess board
+    board = chess.Board(chess960=True)
+    board.set_fen(
+        f"{white.lower()}/pppppppp/8/8/8/8/PPPPPPPP/{black.upper()} w - - 0 1"
+    )
+    logger.info(f"Analyzing position: {white_id=} {black_id=}\n{board.fen()}")
+
+    # Load the analysis configuration
+    cfg = load_config(cfg_id)
 
     # Initialize the Stockfish engine
-    engine = chess.engine.SimpleEngine.popen_uci("stockfish")
+    engine = chess.engine.SimpleEngine.popen_uci(engine_path)
+    logger.info(f"{engine.id=}")
+    if (stockfish_version := engine.id["name"]) != f"Stockfish {cfg.stockfish_version}":
+        raise ValueError(
+            f"Invalid Stockfish version: {stockfish_version} was used, but Stockfish {cfg.stockfish_version} is required"
+        )
 
-    # Create an AnalysisConfig object with desired settings
-    cfg = load_config("XS")
-
-    print(engine.id)
+    # Stockfish settings
+    available_cpus = os.process_cpu_count() or 1
+    _threads: int = threads or max(1, available_cpus - 2)
+    _hash_size: int = hash_size or 1024
+    engine.configure({"Threads": _threads, "Hash": _hash_size})
+    logger.info(f"{_threads=} {_hash_size=}")
 
     # Initialize the RecursiveEngineAnalyzer
     analyzer = RecursiveEngineAnalyzer(board=board, engine=engine, cfg=cfg)
 
-    # Perform analysis (example usage)
+    # Perform analysis
     tree = analyzer.analyse()
+    logger.info(
+        f"Analysis complete for position: {white_id=} {black_id=}\n{board.fen()}"
+    )
+    # Close the engine
+    engine.quit()
+
+    return tree
+
+
+if __name__ == "__main__":
+    tree = analyse_dfrc_position(
+        white_id=518, black_id=518, cfg_id=ConfigId.XS, hash_size=4096
+    )
     print(f"""
           
           -------------------------
@@ -120,6 +169,3 @@ if __name__ == "__main__":
           Analysis tree:
             {tree}
           """)
-
-    # Close the engine
-    engine.quit()
