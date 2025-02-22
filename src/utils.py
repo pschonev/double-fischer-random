a48693fd9e56cfb43c105c7e1905524e526fbba6
Philipp Schoneville <ph.schoneville@gmail.com>
2025-02-22 22:56:44 +0100

    refactor: Change logging format location

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,3 @@
-import logging
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
-)
-
-
 def harmonic_mean(a: float, b: float) -> float:
     """Calculate the harmonic mean of two numbers"""
     return 2 * (a * b) / (a + b)
